391b57bcb3673a6f0aa4c7e9f7d7a5210c44b7db
Khakhalin Andrey Vladimirovich <avkhakhalin@mail.ru>
2021-09-01 22:17:32 +0300

    Выполнен 4 пункт ТЗ.
    Добавлена кнопка в разметку и повешен на неё clickListener в Activity.
    
    Адрес репозитория:
    https://github.com/AVKhakhalin/KotlinHomeWork_1

--- app/src/main/java/ru/geekbrains/lessions2345/kotlinhomework_1/MainActivity.kt
+++ app/src/main/java/ru/geekbrains/lessions2345/kotlinhomework_1/MainActivity.kt
@@ -1,11 +1,29 @@
 package ru.geekbrains.lessions2345.kotlinhomework_1
 
 import android.os.Bundle
+import android.view.View
+import android.widget.Button
+import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
+import androidx.core.view.ViewCompat
 
-class MainActivity : AppCompatActivity() {
+class MainActivity : AppCompatActivity(), View.OnClickListener {
+
+    var button : Button? = null
+
     override fun onCreate(savedInstatnceState : Bundle?) {
         super.onCreate(savedInstatnceState)
         setContentView(R.layout.activity_main)
+
+        button = findViewById(R.id.button)
+        if (button != null) {
+            button?.setOnClickListener(this)
+        }
+    }
+
+    override fun onClick(view : View?) {
+        if ((view != null) && (view?.id == R.id.button)) {
+            Toast.makeText(view.context, "Нажали на кнопку", Toast.LENGTH_SHORT).show()
+        }
     }
 }
