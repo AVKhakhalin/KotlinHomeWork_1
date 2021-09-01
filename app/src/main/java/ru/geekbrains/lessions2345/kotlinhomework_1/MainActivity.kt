78120152d5f7ce7203c649f2bbf34de39d0b476c
Khakhalin Andrey Vladimirovich <avkhakhalin@mail.ru>
2021-09-01 22:03:01 +0300

    Выполнен 3 пункт ТЗ.
    Переведён MainActivity на Kotlin.
    
    Адрес репозитория:
    https://github.com/AVKhakhalin/KotlinHomeWork_1

---
+++ app/src/main/java/ru/geekbrains/lessions2345/kotlinhomework_1/MainActivity.kt
@@ -0,0 +1,11 @@
+package ru.geekbrains.lessions2345.kotlinhomework_1
+
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstatnceState : Bundle?) {
+        super.onCreate(savedInstatnceState)
+        setContentView(R.layout.activity_main)
+    }
+}
